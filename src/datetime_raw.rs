cb355d39bd343bdebeefb69d4469239335f34b63
Petros Angelatos <petrosagg@gmail.com>
2023-02-07 10:24:14 +0100

    better control flow of original goto statements

--- src/datetime_raw.rs
+++ src/datetime_raw.rs
@@ -274,8 +274,8 @@ struct DynamicZoneAbbrev {
     _zone: &'static str,
 }
 
-static DAY_TAB: [i32; 13] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0];
-static LEAP_DAY_TAB: [i32; 13] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0];
+static DAY_TAB: [i32; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+static LEAP_DAY_TAB: [i32; 12] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const EPOCH: &str = "epoch";
 const EARLY: &str = "-infinity";
@@ -294,20 +294,20 @@ struct DateToken {
     value: DateTokenValue,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 enum Millennium {
     AD,
     BC,
 }
 
-#[derive(Debug,PartialEq,Clone)]
+#[derive(Debug, PartialEq, Clone)]
 enum Meridian {
     AM,
     PM,
     HR24,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 enum DateTokenValue {
     FieldType(TokenFieldType),
     Millennium(Millennium),
@@ -1663,97 +1663,116 @@ fn is_valid_julian(year: i32, month: i32) -> bool {
 /// Note: it might seem that we should use mktime() for this, but bitter experience teaches
 /// otherwise.  This code is much faster than most versions of mktime(), anyway.
 fn DetermineTimeZoneOffsetInternal(mut tm: &mut pg_tm, tzp: &pg_tz, tp: &mut pg_time_t) -> i32 {
-    let mut boundary: pg_time_t = 0;
-    let mut before_gmtoff: i64 = 0;
-    let mut after_gmtoff: i64 = 0;
-    let mut before_isdst = false;
-    let mut after_isdst = false;
-    // First, generate the pg_time_t value corresponding to the given
-    // y/m/d/h/m/s taken as GMT time.  If this overflows, punt and decide the
-    // timezone is GMT.  (For a valid Julian date, integer overflow should be
-    // impossible with 64-bit pg_time_t, but let's check for safety.)
-    if !is_valid_julian(tm.tm_year, tm.tm_mon) {
+    fn inner(mut tm: &mut pg_tm, tzp: &pg_tz, tp: &mut pg_time_t) -> Result<i32, ()> {
+        // First, generate the pg_time_t value corresponding to the given y/m/d/h/m/s taken as GMT
+        // time.  If this overflows, punt and decide the timezone is GMT.  (For a valid Julian
+        // date, integer overflow should be impossible with 64-bit pg_time_t, but let's check for
+        // safety.)
+        if !is_valid_julian(tm.tm_year, tm.tm_mon) {
+            return Err(());
+        }
         let date = date2j(tm.tm_year, tm.tm_mon, tm.tm_mday) - (UNIX_EPOCH_JDATE as i32);
+
         let day = date as pg_time_t * SECS_PER_DAY;
-        if day / SECS_PER_DAY == date as i64 {
-            let sec = tm.tm_sec + (tm.tm_min + tm.tm_hour * MINS_PER_HOUR) * SECS_PER_MINUTE;
-            let mytime = day + sec as i64;
-            // since sec >= 0, overflow could only be from +day to -mytime
-            if !(mytime < 0 && day > 0) {
-                // Find the DST time boundary just before or following the target time. We
-                // assume that all zones have GMT offsets less than 24 hours, and that DST
-                // boundaries can't be closer together than 48 hours, so backing up 24
-                // hours and finding the "next" boundary will work.
-                let prevtime = mytime - SECS_PER_DAY;
-                if !(mytime < 0 && prevtime > 0) {
-                    let res = pg_next_dst_boundary(
-                        &prevtime,
-                        &mut before_gmtoff,
-                        &mut before_isdst,
-                        &mut boundary,
-                        &mut after_gmtoff,
-                        &mut after_isdst,
-                        tzp,
-                    );
-                    if res >= 0 {
-                        if res == 0 {
-                            // Non-DST zone, life is simple
-                            tm.tm_isdst = Some(before_isdst);
-                            *tp = mytime - before_gmtoff;
-                            return -(before_gmtoff as i32);
-                        }
-                        // Form the candidate pg_time_t values with local-time adjustment
-                        let beforetime = mytime - before_gmtoff;
-                        if !(before_gmtoff > 0 && mytime < 0 && beforetime > 0
-                            || before_gmtoff <= 0 && mytime > 0 && beforetime < 0)
-                        {
-                            let aftertime = mytime - after_gmtoff;
-                            if !(after_gmtoff > 0 && mytime < 0 && aftertime > 0
-                                || after_gmtoff <= 0 && mytime > 0 && aftertime < 0)
-                            {
-                                // If both before or both after the boundary time, we know what to
-                                // do. The boundary time itself is considered to be after the
-                                // transition, which means we can accept aftertime == boundary in
-                                // the second case.
-                                if beforetime < boundary && aftertime < boundary {
-                                    tm.tm_isdst = Some(before_isdst);
-                                    *tp = beforetime;
-                                    return -(before_gmtoff as i32);
-                                }
-                                if beforetime > boundary && aftertime >= boundary {
-                                    tm.tm_isdst = Some(after_isdst);
-                                    *tp = aftertime;
-                                    return -(after_gmtoff as i32);
-                                }
-                                // It's an invalid or ambiguous time due to timezone transition.
-                                // In a spring-forward transition, prefer the "before"
-                                // interpretation; in a fall-back transition, prefer "after".  (We
-                                // used to define and implement this test as "prefer the
-                                // standard-time interpretation", but that rule does not help to
-                                // resolve the behavior when both times are reported as standard
-                                // time; which does happen, eg Europe/Moscow in Oct 2014.  Also, in
-                                // some zones such as Europe/Dublin, there is widespread confusion
-                                // about which time offset is "standard" time, so it's fortunate
-                                // that our behavior doesn't depend on that.)
-                                if beforetime > aftertime {
-                                    tm.tm_isdst = Some(before_isdst);
-                                    *tp = beforetime;
-                                    return -(before_gmtoff as i32);
-                                }
-                                tm.tm_isdst = Some(after_isdst);
-                                *tp = aftertime;
-                                return -(after_gmtoff as i32);
-                            }
-                        }
-                    }
-                }
-            }
+        if day / SECS_PER_DAY != date as i64 {
+            return Err(());
+        }
+        let sec = tm.tm_sec + (tm.tm_min + tm.tm_hour * MINS_PER_HOUR) * SECS_PER_MINUTE;
+        let mytime = day + sec as i64;
+        // since sec >= 0, overflow could only be from +day to -mytime
+        if mytime < 0 && day > 0 {
+            return Err(());
+        }
+
+        // Find the DST time boundary just before or following the target time. We assume that all
+        // zones have GMT offsets less than 24 hours, and that DST boundaries can't be closer
+        // together than 48 hours, so backing up 24 hours and finding the "next" boundary will
+        // work.
+        let prevtime = mytime - SECS_PER_DAY;
+        if mytime < 0 && prevtime > 0 {
+            return Err(());
+        }
+
+        let mut boundary: pg_time_t = 0;
+        let mut before_gmtoff: i64 = 0;
+        let mut after_gmtoff: i64 = 0;
+        let mut before_isdst = false;
+        let mut after_isdst = false;
+        let res = pg_next_dst_boundary(
+            &prevtime,
+            &mut before_gmtoff,
+            &mut before_isdst,
+            &mut boundary,
+            &mut after_gmtoff,
+            &mut after_isdst,
+            tzp,
+        );
+        if res < 0 {
+            // failure?
+            return Err(());
+        }
+
+        if res == 0 {
+            // Non-DST zone, life is simple
+            tm.tm_isdst = Some(before_isdst);
+            *tp = mytime - before_gmtoff;
+            return Ok(-(before_gmtoff as i32));
+        }
+
+        // Form the candidate pg_time_t values with local-time adjustment
+        let beforetime = mytime - before_gmtoff;
+        if before_gmtoff > 0 && mytime < 0 && beforetime > 0
+            || before_gmtoff <= 0 && mytime > 0 && beforetime < 0
+        {
+            return Err(());
+        }
+        let aftertime = mytime - after_gmtoff;
+        if after_gmtoff > 0 && mytime < 0 && aftertime > 0
+            || after_gmtoff <= 0 && mytime > 0 && aftertime < 0
+        {
+            return Err(());
+        }
+
+        // If both before or both after the boundary time, we know what to do. The boundary time
+        // itself is considered to be after the transition, which means we can accept aftertime ==
+        // boundary in the second case.
+        if beforetime < boundary && aftertime < boundary {
+            tm.tm_isdst = Some(before_isdst);
+            *tp = beforetime;
+            return Ok(-(before_gmtoff as i32));
+        }
+        if beforetime > boundary && aftertime >= boundary {
+            tm.tm_isdst = Some(after_isdst);
+            *tp = aftertime;
+            return Ok(-(after_gmtoff as i32));
+        }
+
+        // It's an invalid or ambiguous time due to timezone transition. In a spring-forward
+        // transition, prefer the "before" interpretation; in a fall-back transition, prefer
+        // "after".  (We used to define and implement this test as "prefer the standard-time
+        // interpretation", but that rule does not help to resolve the behavior when both times are
+        // reported as standard time; which does happen, eg Europe/Moscow in Oct 2014.  Also, in
+        // some zones such as Europe/Dublin, there is widespread confusion about which time offset
+        // is "standard" time, so it's fortunate that our behavior doesn't depend on that.)
+        if beforetime > aftertime {
+            tm.tm_isdst = Some(before_isdst);
+            *tp = beforetime;
+            return Ok(-(before_gmtoff as i32));
+        }
+        tm.tm_isdst = Some(after_isdst);
+        *tp = aftertime;
+        return Ok(-(after_gmtoff as i32));
+    }
+
+    match inner(tm, tzp, tp) {
+        Ok(offset) => offset,
+        Err(_) => {
+            // Given date is out of range, so assume UTC
+            tm.tm_isdst = Some(false);
+            *tp = 0 as pg_time_t;
+            0
         }
     }
-    // Given date is out of range, so assume UTC
-    tm.tm_isdst = Some(false);
-    *tp = 0 as pg_time_t;
-    0
 }
 
 /// Determine the GMT offset and DST flag to be attributed to a dynamic
